c2a217c38fd33140f81c56ea8a6905d44bee360e
tumrod <tipparat.umrod@utexas.edu>
2015-09-12 15:52:04 -0500

    setup sqlite database

---
+++ Item.swift
@@ -0,0 +1,24 @@
+//
+//  Item.swift
+//  
+//
+//  Created by Tipparat Umrod on 9/12/15.
+//
+//
+
+import UIKit
+
+class Item {
+    var itemId: Int
+    var itemName: String
+    
+    init?(itemName:String) {
+        self.itemName = itemName
+        //self.itemId
+        
+        if userName.isEmpty {
+            return nil
+        }
+    }
+    
+}
